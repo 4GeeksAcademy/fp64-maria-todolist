9264426f403947d94a0fc3fb402a4f1ee98eb65a
MaríaSF <158471273+DexpinBe@users.noreply.github.com>
2024-04-27 16:08:09 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import './Home.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+    const [inputState, setInputState] = useState('');
+    const [listaOriginal, setListaOriginal] = useState([{ name: 'Cosas pendientes' }]);
+    const [listaFiltrada, setListaFiltrada] = useState([]);
+    const [textoNuevo, setTextoNuevo] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    useEffect(() => {
+        if (inputState === '') setListaFiltrada(listaOriginal);
+        if (inputState !== '') {
+            const estadoActualizado = listaOriginal.filter((item) => {
+                return item.name.toLowerCase().includes(inputState.toLowerCase());
+            });
+            setListaFiltrada(estadoActualizado);
+        }
+    }, [listaOriginal, inputState]);
+
+    const agregarNuevoMiembro = () => {
+        setListaOriginal([...listaOriginal, { name: textoNuevo }]);
+        setTextoNuevo('');
+    };
+
+    const eliminarTarea = (index) => {
+        const nuevaLista = listaOriginal.filter((_, i) => i !== index);
+        setListaOriginal(nuevaLista);
+    };
+
+    return (
+        <div className="background">
+            <div className="task-list-container">
+                <div className="task-list">
+                    <input
+                        type="text"
+                        name=""
+                        id=""
+                        onChange={(event) => setTextoNuevo(event.target.value)}
+                        value={textoNuevo}
+                    />
+                    <button className='agregar' onClick={agregarNuevoMiembro}>Agregar</button>
+
+                    <ul>
+                        {listaFiltrada.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    {item.name}
+                                    <button className="eliminar" onClick={() => eliminarTarea(index)}><i className="fas fa-times"></i></button>
+                                </li>
+                            );
+                        })}
+                    </ul>
+					<div className="task-counter">
+                Tareas: {listaFiltrada.length}
+            </div>
+                </div>
+				
+            </div>
+			
+        </div>
+    );
+}
 
 export default Home;
